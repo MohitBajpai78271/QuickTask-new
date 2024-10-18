741c1ac5f2591a003d1f5599bce04599a76a185d
MohitBajpai78271 <mohitbajpai997@gmail.com>
2024-10-18 13:17:04 +0530

    Initial Commit

---
+++ QuickTaskTests/QuickTaskTests.swift
@@ -0,0 +1,17 @@
+//
+//  QuickTaskTests.swift
+//  QuickTaskTests
+//
+//  Created by Mac on 18/10/24.
+//
+
+import Testing
+@testable import QuickTask
+
+struct QuickTaskTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
